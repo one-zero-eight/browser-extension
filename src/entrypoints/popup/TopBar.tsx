c2b3e05cdeecff40f4e809c03fb0a1aa97603fa7
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-06-21 22:52:16 +0300

    chore: change title in popup

--- src/entrypoints/popup/TopBar.tsx
+++ src/entrypoints/popup/TopBar.tsx
@@ -1,7 +1,7 @@
 export function TopBar() {
   return (
     <div className="h-12 flex items-center justify-between bg-black p-2">
-      <div className="text-xl">InNoHassle Extension</div>
+      <div className="text-xl">InNoHassle Tools</div>
       <button type="button" onClick={() => chrome.runtime.openOptionsPage()} className="i-material-symbols-settings-rounded text-xl" />
     </div>
   )
