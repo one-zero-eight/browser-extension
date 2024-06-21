0c16c953e325cb1a0a7eb08d3f9bc66a6f11b136
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-06-21 22:53:16 +0300

    chore: separate building scripts for Chrome and Firefox

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,12 +1,22 @@
 import { createRequire } from 'node:module'
+import process from 'node:process'
 import gulp from 'gulp'
 import zip from 'gulp-zip'
 
 const require = createRequire(import.meta.url)
 const manifest = require('../build/manifest.json')
 
+const postfix = process.argv[2] ? `-${process.argv[2]}` : ''
+
+const isFirefox = process.env.BROWSER === 'firefox'
+const browserPostfix = isFirefox ? '-firefox' : '-chrome'
+
+const dashedName = manifest.name.replaceAll(' ', '-')
+const fileName = `${dashedName}-${manifest.version}${postfix}${browserPostfix}.zip`
+
 gulp
   .src('build/**')
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
+  .pipe(zip(fileName))
   .pipe(gulp.dest('package'))
-console.log(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`)
+
+console.log(fileName)
